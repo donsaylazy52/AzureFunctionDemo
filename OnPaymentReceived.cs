16e68996171cb9dde021dc6226083414fd3290c2
鐵輪兄 <120096@o365.tku.edu.tw>
2020-06-28 17:52:24 +0800

    alter default codes

--- OnPaymentReceived.cs
+++ OnPaymentReceived.cs
@@ -14,22 +14,15 @@ namespace azFunctionDemo
     {
         [FunctionName("OnPaymentReceived")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
+            log.LogInformation("Received a payment.");
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult($"Thank you for your purchase");
         }
     }
 }
